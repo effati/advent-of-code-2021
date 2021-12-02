57fa2ea7f7233c44cb1371439225131807b9d6f3
effati <shayan@effati.com>
2021-12-02 17:37:00 +0100

    Day 1 update

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,15 +1,9 @@
 object Day1 {
-  def main(args: Array[String]): Unit = {
-    val input = Utils.read("input1").map(_.toInt)
-    println(problem1(input))
-    println(problem2(input))
-  }
-
   def problem1(input: List[Int]): Int = {
     input
       .sliding(2)
-      .map(pair => if (pair(0) < pair(1)) 1 else 0)
-      .sum
+      .map(pair => pair(0) < pair(1))
+      .count(a => a)
   }
 
   def problem2(input: List[Int]): Int = {
@@ -18,5 +12,11 @@ object Day1 {
       .map(_.sum)
 
     problem1(windowSums.toList)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = Utils.read("input1").map(_.toInt)
+    println(problem1(input))
+    println(problem2(input))
   }
 }
