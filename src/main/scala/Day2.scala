3f25219f670abb6d07c39e23fa29287c0b1203d8
effati <shayan@effati.com>
2021-12-02 17:54:41 +0100

    Day 2 update

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -1,29 +1,27 @@
 object Day2 {
 
   def problem1(input: List[(String, Int)]): Int = {
-    val res = input.map { line =>
-      line._1 match {
-        case "forward" => (line._2, 0)
-        case "up" => (0, -line._2)
-        case "down" => (0, line._2)
+    val res = input.foldLeft((0, 0)) { (acc, curr) =>
+      curr._1 match {
+        case "forward" => (acc._1 + curr._2, acc._2)
+        case "up" => (acc._1, acc._2 - curr._2)
+        case "down" => (acc._1, acc._2 + curr._2)
       }
-    }.foldLeft((0, 0)) { case (acc, (horizontal, vertical)) => (acc._1 + horizontal, acc._2 + vertical) }
+    }
 
     res._1 * res._2
   }
 
   def problem2(input: List[(String, Int)]): Int = {
-    val res = input.scanLeft((0, 0, 0)) { (acc, curr) =>
-      val (horizontal, vertical, aim) = acc
-      val (direction, units) = curr
-      direction match {
-        case "forward" => (horizontal + units, vertical + (aim * units), aim)
-        case "up" => (horizontal, vertical, aim - units)
-        case "down" => (horizontal, vertical, aim + units)
+    val res = input.foldLeft((0, 0, 0)) { (acc, curr) =>
+      curr._1 match {
+        case "forward" => (acc._1 + curr._2, acc._2 + (acc._3 * curr._2), acc._3)
+        case "up" => (acc._1, acc._2, acc._3 - curr._2)
+        case "down" => (acc._1, acc._2, acc._3 + curr._2)
       }
     }
 
-    res.last._1 * res.last._2
+    res._1 * res._2
   }
 
   def main(args: Array[String]): Unit = {
