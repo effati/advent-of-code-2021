a61319f1571eae2962d14d13b5b22e357bd873cb
effati <shayan@effati.com>
2021-12-10 14:14:55 +0100

    Day 10 cleaned

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -2,42 +2,29 @@ import scala.annotation.tailrec
 
 object Day10 {
   val Matching = Map('(' -> ')', '[' -> ']', '{' -> '}', '<' -> '>')
-  val SyntaxErrorScores = Map(
-    ')' -> 3,
-    ']' -> 57,
-    '}' -> 1197,
-    '>' -> 25137
-  )
-  val ClosingPoints = Map(
-    ')' -> 1,
-    ']' -> 2,
-    '}' -> 3,
-    '>' -> 4
-  )
+  val SyntaxErrorScores = Map(')' -> 3, ']' -> 57, '}' -> 1197, '>' -> 25137)
+  val ClosingPoints = Map(')' -> 1, ']' -> 2, '}' -> 3, '>' -> 4)
 
   @tailrec
   def findError(chars: List[Char], stack: List[Char]): Option[Char] = {
     val (curr, rest) = (chars.head, chars.drop(1))
-    if (Matching.contains(curr)) {
+    if (Matching.contains(curr))
       if (rest.isEmpty) None else findError(rest, curr :: stack)
-    } else {
+    else {
       val (pop, tail) = (stack.head, stack.drop(1))
-      if (Matching.map(_.swap)(curr) == pop) {
+      if (Matching.map(_.swap)(curr) == pop)
         if (tail.isEmpty || rest.isEmpty) None else findError(rest, tail)
-      } else {
-        Some(curr)
-      }
+      else Some(curr)
     }
   }
 
   @tailrec
   def findOpen(chars: List[Char], stack: List[Char]): List[Char] = {
     val (curr, rest) = (chars.head, chars.drop(1))
-    if (Matching.contains(curr)) {
+    if (Matching.contains(curr))
       if (rest.isEmpty) curr :: stack else findOpen(rest, curr :: stack)
-    } else {
-      if (rest.isEmpty) stack.tail else findOpen(rest, stack.tail)
-    }
+    else if (rest.isEmpty) stack.tail
+    else findOpen(rest, stack.tail)
   }
 
   def problem1(input: List[List[Char]]): Int = {
