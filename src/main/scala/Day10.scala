5f2aa625c7b666768e4f885471cbcabe51b9208d
effati <shayan@effati.com>
2021-12-10 14:26:20 +0100

    Day 10 cleaned

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -6,40 +6,36 @@ object Day10 {
   val ClosingPoints = Map(')' -> 1, ']' -> 2, '}' -> 3, '>' -> 4)
 
   @tailrec
-  def findError(chars: List[Char], stack: List[Char]): Option[Char] = {
+  def findError(chars: List[Char], stack: List[Char] = List()): Option[Char] = {
     val (curr, rest) = (chars.head, chars.drop(1))
-    if (Matching.contains(curr))
-      if (rest.isEmpty) None else findError(rest, curr :: stack)
+    if (Matching.contains(curr)) if (rest.isEmpty) None else findError(rest, curr :: stack)
     else {
       val (pop, tail) = (stack.head, stack.drop(1))
-      if (Matching.map(_.swap)(curr) == pop)
-        if (tail.isEmpty || rest.isEmpty) None else findError(rest, tail)
+      if (Matching.map(_.swap)(curr) == pop) if (tail.isEmpty || rest.isEmpty) None else findError(rest, tail)
       else Some(curr)
     }
   }
 
   @tailrec
-  def findOpen(chars: List[Char], stack: List[Char]): List[Char] = {
+  def findOpen(chars: List[Char], stack: List[Char] = List()): List[Char] = {
     val (curr, rest) = (chars.head, chars.drop(1))
-    if (Matching.contains(curr))
-      if (rest.isEmpty) curr :: stack else findOpen(rest, curr :: stack)
+    if (Matching.contains(curr)) if (rest.isEmpty) curr :: stack else findOpen(rest, curr :: stack)
     else if (rest.isEmpty) stack.tail
     else findOpen(rest, stack.tail)
   }
 
   def problem1(input: List[List[Char]]): Int = {
     input
-      .flatMap(row => findError(row, List()))
+      .flatMap(findError(_))
       .map(SyntaxErrorScores)
       .sum
   }
 
   def problem2(input: List[List[Char]]): Long = {
-    val sorted = input
-      .filter(row => findError(row, List()).isEmpty)
-      .map(row => findOpen(row, List()).foldLeft(0L)((acc, char) => acc * 5 + ClosingPoints(Matching(char))))
-      .sorted
-    sorted.drop(sorted.size / 2).head
+    val output = input
+      .filter(findError(_).isEmpty)
+      .map(findOpen(_).foldLeft(0L)((acc, char) => acc * 5 + ClosingPoints(Matching(char))))
+    output.sorted.drop(output.size / 2).head
   }
 
   def main(args: Array[String]): Unit = {
