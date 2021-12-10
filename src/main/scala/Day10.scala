3fbed71180e019a301860d877264966511a8fe43
effati <shayan@effati.com>
2021-12-10 11:11:28 +0100

    Day 10 cleaned

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -2,89 +2,61 @@ import scala.annotation.tailrec
 
 object Day10 {
   val Matching = Map('(' -> ')', '[' -> ']', '{' -> '}', '<' -> '>')
-
   val SyntaxErrorScores = Map(
     ')' -> 3,
     ']' -> 57,
     '}' -> 1197,
     '>' -> 25137
   )
-
   val ClosingPoints = Map(
-  ')' -> 1,
-  ']' -> 2,
-'}' -> 3,
+    ')' -> 1,
+    ']' -> 2,
+    '}' -> 3,
     '>' -> 4
-)
+  )
 
-  def problem1(input: List[String]): Int = {
-    input.flatMap { row =>
-      findError(row.toList, List())
+  @tailrec
+  def findError(chars: List[Char], stack: List[Char]): Option[Char] = {
+    val (curr, rest) = (chars.head, chars.drop(1))
+    if (Matching.contains(curr)) {
+      if (rest.isEmpty) None else findError(rest, curr :: stack)
+    } else {
+      val (pop, tail) = (stack.head, stack.drop(1))
+      if (Matching.map(_.swap)(curr) == pop) {
+        if (tail.isEmpty || rest.isEmpty) None else findError(rest, tail)
+      } else {
+        Some(curr)
+      }
     }
+  }
+
+  @tailrec
+  def findOpen(chars: List[Char], stack: List[Char]): List[Char] = {
+    val (curr, rest) = (chars.head, chars.drop(1))
+    if (Matching.contains(curr)) {
+      if (rest.isEmpty) curr :: stack else findOpen(rest, curr :: stack)
+    } else {
+      if (rest.isEmpty) stack.tail else findOpen(rest, stack.tail)
+    }
+  }
+
+  def problem1(input: List[List[Char]]): Int = {
+    input
+      .flatMap(row => findError(row, List()))
       .map(SyntaxErrorScores)
       .sum
   }
 
-
-  @tailrec
-  def findError(chars: List[Char], stack: List[Char]): Option[Char] = {
-    if (chars.size <= 1) {
-      val curr = chars.head
-      if (Matching.contains(curr)) {
-        None
-      } else {
-        val pop = stack.head
-        if (Matching.map(_.swap)(curr) == pop) {
-          None
-        } else {
-          Some(curr)
-        }
-      }
-    } else {
-      val curr :: rest = chars
-      if (Matching.contains(curr)) {
-        findError(rest, curr :: stack)
-      } else {
-        val pop :: tail = stack
-        if (Matching.map(_.swap)(curr) == pop) {
-          findError(rest, tail)
-        } else {
-          Some(curr)
-        }
-      }
-    }
-  }
-
-  def findOpen(chars: List[Char], stack: List[Char]): List[Char] = {
-    if (chars.size <= 1) {
-      val curr = chars.head
-      if (Matching.contains(curr)) {
-        curr :: stack
-      } else {
-        stack.tail
-      }
-    } else {
-      val curr :: rest = chars
-      if (Matching.contains(curr)) {
-        findOpen(rest, curr :: stack)
-      } else {
-        findOpen(rest, stack.tail)
-      }
-    }
-  }
-
-  def problem2(input: List[String]): Long = {
-    val a = input
-      .map(_.toList)
+  def problem2(input: List[List[Char]]): Long = {
+    val sorted = input
       .filter(row => findError(row, List()).isEmpty)
-      .map(row => findOpen(row, List()))
-      .map(row => row.foldLeft(0L)((a, char) => a * 5 + ClosingPoints(Matching(char))))
+      .map(row => findOpen(row, List()).foldLeft(0L)((acc, char) => acc * 5 + ClosingPoints(Matching(char))))
       .sorted
-    a.drop(a.size/2).head
+    sorted.drop(sorted.size / 2).head
   }
 
   def main(args: Array[String]): Unit = {
-    val input = Utils.read("input10")
+    val input = Utils.read("input10").map(_.toList)
     println(problem1(input))
     println(problem2(input))
   }
