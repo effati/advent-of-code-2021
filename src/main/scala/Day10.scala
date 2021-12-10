6e0b85bd0113fd9dc404bebfa7e13939986f434e
effati <shayan@effati.com>
2021-12-10 10:38:33 +0100

    Day 10 progress

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -7,12 +7,18 @@ object Day10 {
     ')' -> 3,
     ']' -> 57,
     '}' -> 1197,
-    '>' -> 25137,
-    ' ' -> 0
+    '>' -> 25137
   )
 
+  val ClosingPoints = Map(
+  ')' -> 1,
+  ']' -> 2,
+'}' -> 3,
+    '>' -> 4
+)
+
   def problem1(input: List[String]): Int = {
-    input.map { row =>
+    input.flatMap { row =>
       findError(row.toList, List())
     }
       .map(SyntaxErrorScores)
@@ -21,17 +27,17 @@ object Day10 {
 
 
   @tailrec
-  def findError(chars: List[Char], stack: List[Char]): Char = {
+  def findError(chars: List[Char], stack: List[Char]): Option[Char] = {
     if (chars.size <= 1) {
       val curr = chars.head
       if (Matching.contains(curr)) {
-        ' '
+        None
       } else {
         val pop = stack.head
         if (Matching.map(_.swap)(curr) == pop) {
-          ' '
+          None
         } else {
-          curr
+          Some(curr)
         }
       }
     } else {
@@ -43,16 +49,38 @@ object Day10 {
         if (Matching.map(_.swap)(curr) == pop) {
           findError(rest, tail)
         } else {
-          curr
+          Some(curr)
         }
       }
     }
   }
 
-  def problem2(input: List[String]): Int = {
-    input.filter { row =>
-      findError(row.toList, List())
+  def findOpen(chars: List[Char], stack: List[Char]): List[Char] = {
+    if (chars.size <= 1) {
+      val curr = chars.head
+      if (Matching.contains(curr)) {
+        curr :: stack
+      } else {
+        stack.tail
+      }
+    } else {
+      val curr :: rest = chars
+      if (Matching.contains(curr)) {
+        findOpen(rest, curr :: stack)
+      } else {
+        findOpen(rest, stack.tail)
+      }
     }
+  }
+
+  def problem2(input: List[String]): Long = {
+    val a = input
+      .map(_.toList)
+      .filter(row => findError(row, List()).isEmpty)
+      .map(row => findOpen(row, List()))
+      .map(row => row.foldLeft(0L)((a, char) => a * 5 + ClosingPoints(Matching(char))))
+      .sorted
+    a.drop(a.size/2).head
   }
 
   def main(args: Array[String]): Unit = {
