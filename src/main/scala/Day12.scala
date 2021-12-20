82c0d472c63dcf97a8b146c339007236ea093f65
effati <shayan@effati.com>
2021-12-20 20:59:20 +0100

    Day 12 full solution

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -1,11 +1,8 @@
 object Day12 {
   type Graph = Map[String, List[String]]
-  type HashGraph = Map[Int, List[Int]]
 
   val start = "start"
   val destination = "end"
-  val hashStart = start.hashCode
-  val hashDest = destination.hashCode
 
   def problem1(graph: Graph): Int = {
     def count(cave: String, visited: List[String]): Int = {
@@ -16,21 +13,21 @@ object Day12 {
     count(start, List())
   }
 
-  def problem2(graph: HashGraph, lowerCaves: Set[Int]): Int = {
+  def problem2(graph: Graph): Int = {
     def count(
-      cave: Int,
-      visited: List[Int],
-      visitedSmallCaveTwice: Int
+      cave: String,
+      visited: List[String],
+      visitedSmallCaveTwice: String
     ): Int = {
-      val isSmallAndVisited = lowerCaves.contains(cave) && visited.contains(cave)
-      if (cave == hashDest) 1
-      else if (isSmallAndVisited && (cave == hashStart || visitedSmallCaveTwice != -1)) 0
+      val isSmallAndVisited = cave.head.isLower && visited.contains(cave)
+      if (cave == destination) 1
+      else if (isSmallAndVisited && (cave == start || visitedSmallCaveTwice != "")) 0
       else
         graph(cave)
           .map(count(_, cave :: visited, if (isSmallAndVisited) cave else visitedSmallCaveTwice))
           .sum
     }
-    count(hashStart, List(), -1)
+    count(start, List(), "")
   }
 
   def main(args: Array[String]): Unit = {
@@ -41,15 +38,7 @@ object Day12 {
     val graph = (input ++ input.map(_.swap)).groupMap(_._1)(_._2)
 
     println(problem1(graph))
-
-    val input2 = Utils.read("input12").map { s =>
-      val edge = s.split('-')
-      edge.head.hashCode -> edge.last.hashCode
-    }
-    val graph2 = (input2 ++ input2.map(_.swap)).groupMap(_._1)(_._2)
-    val lowerCaves = input.map(_._1).filter(_.head.isLower).map(_.hashCode).toSet
-
-    println(problem2(graph2, lowerCaves))
+    println(problem2(graph))
   }
 
 }
