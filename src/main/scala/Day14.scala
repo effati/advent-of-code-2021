769be2e290b8cf24cc03577d6eea7526b91e4e80
effati <shayan@effati.com>
2021-12-20 23:19:57 +0100

    Day 14 complete

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -1,27 +1,58 @@
 object Day14 {
-  def problem1(template: String, rules: Map[String, String]): Int = {
-    val res = (0 until 10)
-      .foldLeft(template) { (polymer, _) =>
-        polymer
-          .sliding(2)
-          .map { a =>
-            val insertion = rules(a)
-            a.head + insertion
-          }
-          .toSeq
-          .mkString :+ polymer.last
-      }
-      .groupBy(identity)
-      .map(a => (a._1, a._2.length))
+  type Rules = Map[(Char, Char), Char]
+  type CharCount = Map[Char, Long]
+  type PairCount = Map[(Char, Char), Long]
 
-    res.maxBy(_._2)._2 - res.minBy(_._2)._2
+  def pairInsertion(
+    rules: Rules,
+    initCharCount: CharCount,
+    initPairCount: PairCount,
+    steps: Int
+  ): Long = {
+    val (count, _) = (0 until steps)
+      .foldLeft((initCharCount, initPairCount)) {
+        case ((charCount, pairCount), _) =>
+          step(rules, charCount, pairCount)
+      }
+    val vals = count.values.toList
+    vals.max - vals.min
+  }
+
+  def step(
+    rules: Rules,
+    charCount: CharCount,
+    pairCount: PairCount
+  ): (CharCount, PairCount) = {
+    pairCount.foldLeft((charCount, Map[(Char, Char), Long]())) {
+      case ((charCount2, pairCount2), (pair, count)) =>
+        val (a, b) = pair
+        val insertion = rules(pair)
+
+        (charCount2 + (insertion -> (charCount2.getOrElse(insertion, 0L) + count))) -> (pairCount2 ++ Map(
+          (a, insertion) -> (count + pairCount2.getOrElse((a, insertion), 0L)),
+          (insertion, b) -> (count + pairCount2.getOrElse((insertion, b), 0L))
+        ))
+    }
   }
 
   def main(args: Array[String]): Unit = {
     val input = Utils.read("input14")
-    val template = input.head
-    val rules = input.tail.filterNot(_.isBlank).map(_.split(" -> ")).map(rule => (rule.head, rule.last)).toMap
+    val initCharCount = input.head.groupBy(identity).map { case (mol, mols) => mol -> mols.length.toLong }
+    val initPairCount = input.head
+      .sliding(2)
+      .map(_.toList)
+      .map { a => (a.head, a.last) }
+      .toList
+      .groupBy(identity)
+      .map(a => (a._1, a._2.length.toLong))
 
-    println(problem1(template, rules))
+    val rules = input.tail
+      .filterNot(_.isBlank)
+      .map(_.split(" -> "))
+      .map(rule => (rule.head.head, rule.head.last) -> rule.last.head)
+      .toMap
+
+    println(pairInsertion(rules, initCharCount, initPairCount, 10))
+    println(pairInsertion(rules, initCharCount, initPairCount, 40))
   }
 }
