680a408e3ac8b6e2ff6d6e9801aff82b37f6c805
effati <shayan@effati.com>
2021-12-20 12:09:49 +0100

    Day 12 problem 2 init

--- src/main/scala/Day12.scala
+++ src/main/scala/Day12.scala
@@ -1,26 +1,44 @@
 object Day12 {
   type Graph = Map[String, List[String]]
-  
-  def problem1(input: Graph): Int = count(input, "start", "end")
 
-  def count(graph: Graph, start: String, destination: String): Int = {
-    def count0(cave: String, visited: List[String]): Int = {
+  val start = "start"
+  val destination = "end"
+
+  def problem1(graph: Graph): Int = {
+    def count(cave: String, visited: List[String]): Int = {
       if (cave == destination) 1
       else if (visited.contains(cave) && cave.head.isLower) 0
-      else {
-        graph(cave).map(neighbor => count0(neighbor, cave :: visited)).sum
-      }
+      else graph(cave).map(neighbor => count(neighbor, cave :: visited)).sum
     }
-    count0(start, List())
+    count(start, List())
+  }
+
+  def problem2(graph: Graph): Int = {
+    def count(
+      cave: String,
+      visited: List[String] = List(),
+      visitedSmallCaveTwice: String = ""
+    ): Int = {
+      val isSmallAndVisited = cave.head.isLower && visited.contains(cave)
+      if (cave == destination) 1
+      else if (isSmallAndVisited && (cave == start || (visitedSmallCaveTwice != "" && cave != visitedSmallCaveTwice))) 0
+      else
+        graph(cave)
+          .map(count(_, cave :: visited, if (isSmallAndVisited) cave else visitedSmallCaveTwice))
+          .sum
+    }
+    count(start)
   }
 
   def main(args: Array[String]): Unit = {
-    val input = Utils.read("input12").map{ s => 
+    val input = Utils.read("input12").map { s =>
       val edge = s.split('-')
       edge.head -> edge.last
     }
     val graph = (input ++ input.map(_.swap)).groupMap(_._1)(_._2)
+
     println(problem1(graph))
+    println(problem2(graph))
   }
 
 }
