769cb882816715c50402049df5238e579205feb4
effati <shayan@effati.com>
2021-12-23 08:50:06 +0100

    Day 15 formatted

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -31,16 +31,21 @@ object Day15 {
 
   def dijsktra2(graph: Graph, source: Vertex): (Graph, Map[Vertex, Vertex]) = {
     @tailrec
-    def go(active: Set[Vertex], res: Graph, pred: Map[Vertex, Vertex]): (Graph, Map[Vertex, Vertex]) = {
+    def go(
+      active: Set[Vertex],
+      res: Graph,
+      pred: Map[Vertex, Vertex]
+    ): (Graph, Map[Vertex, Vertex]) = {
       if (active.isEmpty) (res, pred)
       else {
         val node = active.minBy(res)
         val cost = res(node)
         val neighbors = graph.keys.filter(Neighboring.map { case (dx, dy) => (dx + node._1, dy + node._2) }.contains)
-        val alt = {for {
-          n <- neighbors if
-            cost + graph(n) < res.getOrElse(n, Int.MaxValue)
-        } yield n -> (cost + graph(n))}.toMap
+        val alt = {
+          for {
+            n <- neighbors if cost + graph(n) < res.getOrElse(n, Int.MaxValue)
+          } yield n -> (cost + graph(n))
+        }.toMap
         val active1 = active - node ++ alt.keys
         val preds = alt.keys.map(key => key -> node).toMap
         go(active1, res ++ alt, pred ++ preds)
@@ -50,11 +55,14 @@ object Day15 {
   }
 
   def main(args: Array[String]): Unit = {
-    val input: Graph = Utils.read("input15").map(_.map(_.toString.toInt).toList)
+    val input: Graph = Utils
+      .read("input15")
+      .map(_.map(_.toString.toInt).toList)
       .zipWithIndex
-      .flatMap { case (row, x) =>
-        row.zipWithIndex
-        .map { case (col, y) => (x, y) -> col}
+      .flatMap {
+        case (row, x) =>
+          row.zipWithIndex
+            .map { case (col, y) => (x, y) -> col }
       }
       .toMap
 
@@ -67,25 +75,27 @@ object Day15 {
     val input2 = Utils.read("input15").map(_.map(_.toString.toInt).toList)
 
     val b = (0 until 5).flatMap { i =>
-      input2.map(_.map {
-        value => val newVal = value + i
-          if (newVal > 9) (newVal % 10) + 1 else newVal
+      input2.map(_.map { value =>
+        val newVal = value + i
+        if (newVal > 9) (newVal % 10) + 1 else newVal
       })
     }.toList
 
     val c =
       b.map(row =>
-        (0 until 5).flatMap( i => row.map {
-        value => val newVal = value + i
-          if (newVal > 9) (newVal % 10) + 1 else newVal
-      }))
+        (0 until 5).flatMap(i =>
+          row.map { value =>
+            val newVal = value + i
+            if (newVal > 9) (newVal % 10) + 1 else newVal
+          }
+        )
+      )
 
-    val a: Graph = c.zipWithIndex
-      .flatMap { case (row, x) =>
+    val a: Graph = c.zipWithIndex.flatMap {
+      case (row, x) =>
         row.zipWithIndex
-          .map { case (col, y) => (x, y) -> col}
-      }
-      .toMap
+          .map { case (col, y) => (x, y) -> col }
+    }.toMap
 
     val (dist2, _) = dijsktra2(a, (0, 0))
     val vals2 = dist2.keys
