65628579676743e53e4cc3857c25979bf84fb747
effati <shayan@effati.com>
2021-12-24 01:05:18 +0100

    Day 17 cleaned

--- src/main/scala/Day17.scala
+++ src/main/scala/Day17.scala
@@ -2,89 +2,62 @@ import scala.annotation.tailrec
 
 object Day17 {
   case class Coordinate(x: Int, y: Int)
-  case class Trajectory(x: Int, y: Int)
+  case class Velocity(x: Int, y: Int)
+  case class Area(
+    x1: Int,
+    x2: Int,
+    y1: Int,
+    y2: Int
+  )
   val Start = Coordinate(0, 0)
 
-  def tryThis(trajectory: Trajectory, xRange: Range, yRange: Range): Option[List[Coordinate]] = {
+  def extractHits(velocity: Velocity, area: Area): Option[List[Coordinate]] = {
     @tailrec
-    def go(coordinate: Coordinate, trajectory: Trajectory, positions: List[Coordinate]): Option[List[Coordinate]] = {
-      if (coordinate.x > xRange.last || coordinate.y < yRange.head) None
-      else if (xRange.contains(coordinate.x) && yRange.contains(coordinate.y)) Some(positions)
+    def go(coordinate: Coordinate, velocity: Velocity, probeHistory: List[Coordinate]): Option[List[Coordinate]] = {
+      if (coordinate.x > area.x2 || coordinate.y < area.y1) None
+      else if (area.x1 <= coordinate.x && coordinate.y <= area.y2) Some(probeHistory)
       else {
-        val newCoord = Coordinate(coordinate.x + trajectory.x, coordinate.y + trajectory.y)
-        val newTraj = Trajectory(
-          if (trajectory.x > 0) trajectory.x - 1 else if (trajectory.x < 0) trajectory.x + 1 else 0,
-          trajectory.y - 1
+        val nextCoordinate = Coordinate(coordinate.x + velocity.x, coordinate.y + velocity.y)
+        val nextVelocity = Velocity(
+          if (velocity.x > 0) velocity.x - 1 else if (velocity.x < 0) velocity.x + 1 else 0,
+          velocity.y - 1
         )
-        go(newCoord, newTraj, positions :+ coordinate)
+        go(nextCoordinate, nextVelocity, probeHistory :+ nextCoordinate)
       }
     }
-    go(Start, trajectory, List())
+    go(Start, velocity, List(Start))
   }
 
-  def tryThis2(
-    trajectory: Trajectory,
-    xStart: Int,
-    xEnd: Int,
-    yStart: Int,
-    yEnd: Int
-  ): Option[Trajectory] = {
-    @tailrec
-    def go(coordinate: Coordinate, trajectory: Trajectory): Option[Trajectory] = {
-      if (coordinate.x > xEnd || coordinate.y < yStart) None
-      else if (xStart <= coordinate.x && coordinate.y <= yEnd) Some(trajectory)
-      else {
-        val newCoord = Coordinate(coordinate.x + trajectory.x, coordinate.y + trajectory.y)
-        val newTraj = Trajectory(
-          if (trajectory.x > 0) trajectory.x - 1 else if (trajectory.x < 0) trajectory.x + 1 else 0,
-          trajectory.y - 1
-        )
-        go(newCoord, newTraj)
-      }
-    }
-    go(Start, trajectory)
+  def hits(input: String): Seq[(Velocity, List[Coordinate])] = {
+    val p = ".*x=(-?\\d+)..(-?\\d+), y=(-?\\d+)..(-?\\d+)".r
+    val p(x1, x2, y1, y2) = input
+    val area = Area(x1.toInt, x2.toInt, y1.toInt, y2.toInt)
+
+    val combinations = List((1, 1), (1, -1), (-1, 1), (-1, -1))
+    (0 to area.x2)
+      .flatMap(x =>
+        (0 to Math.abs(area.y1)).flatMap(y => combinations.map { case (dx, dy) => Velocity(x * dx, y * dy) })
+      )
+      .flatMap(velocity =>
+        extractHits(velocity, area) match {
+          case Some(res) => Some((velocity, res))
+          case _         => None
+        }
+      )
   }
 
   def problem1(input: String): Int = {
-    val p = ".*x=(-?\\d+)..(-?\\d+), y=(-?\\d+)..(-?\\d+)".r
-    val p(xStart, xEnd, yStart, yEnd) = input
-    val (xRange, yRange) = (xStart.toInt to xEnd.toInt, yStart.toInt to yEnd.toInt)
-
-    LazyList
-      .from(0)
-      .map { i =>
-        (0 to i)
-          .flatMap(x =>
-            (0 to i)
-              .map(y => Trajectory(x, y))
-          )
-          .flatMap(tryThis(_, xRange, yRange))
-      }
-      .dropWhile(_.isEmpty)
-      .take(200)
-      .flatten
-      .flatten
+    hits(input)
+      .map(_._2)
       .distinct
+      .flatten
       .maxBy(_.y)
       .y
   }
 
   def problem2(input: String): Int = {
-    val p = ".*x=(-?\\d+)..(-?\\d+), y=(-?\\d+)..(-?\\d+)".r
-    val p(x1, x2, y1, y2) = input
-    val (xStart, xEnd, yStart, yEnd) = (x1.toInt, x2.toInt, y1.toInt, y2.toInt)
-
-    val combinations = List((1, 1), (1, -1), (-1, 1), (-1, -1))
-    val i = 1000
-    (0 to i)
-      .flatMap(x =>
-        (0 to i)
-          .flatMap(y => combinations.map { case (dx, dy) => Trajectory(x * dx, y * dy) })
-      )
-      .flatMap { traj =>
-        val a = tryThis2(traj, xStart, xEnd, yStart, yEnd)
-        if (a.nonEmpty) Some(traj) else None
-      }
+    hits(input)
+      .map(_._1)
       .distinct
       .length
   }
