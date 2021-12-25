725c107fbde77f1e7d9cbf470c07eb476bc58c2e
effati <shayan@effati.com>
2021-12-25 21:11:12 +0100

    Day 18 problem 1 working

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -1,83 +1,81 @@
 import scala.annotation.tailrec
 
 object Day18 {
-  case class Node(n: Int, level: Int)
-
-  def problem1(input: Seq[String]): Int = {
-    val nodes = input.map(binTree)
-    magnitude(
-      nodes.tail.foldLeft(nodes.head) { (acc, node) =>
-        println(acc)
-        reduce(join(acc, node))
-      }
-    )
+  sealed trait Node {
+    def explode(depth: Int): ExplodeResult
+    def split: Node
+    def magnitude: Int
+    def addLeft(value: Int): Node
+    def addRight(value: Int): Node
   }
+  case class Pair(left: Node, right: Node) extends Node {
+    override def explode(depth: Int): ExplodeResult = {
+      (left, right) match {
+        case (Number(l), Number(r)) if depth <= 0 => ExplodeResult(Number(0), l, r)
+        case _ =>
+          val leftExp = left.explode(depth - 1)
+          val nr = if (leftExp.addRight != 0) right.addLeft(leftExp.addRight) else right
+          val rightExp = nr.explode(depth - 1)
+          val nl = if (rightExp.addLeft != 0) leftExp.node.addRight(rightExp.addLeft) else leftExp.node
+          ExplodeResult(Pair(nl, rightExp.node), leftExp.addLeft, rightExp.addRight)
+      }
+    }
+
+    override def split: Node = {
+      val nl = left.split
+      if (nl != left) Pair(nl, right) else Pair(left, right.split)
+    }
+    override def magnitude: Int = 3 * left.magnitude + 2 * right.magnitude
+    override def addLeft(value: Int): Node = Pair(left.addLeft(value), right)
+    override def addRight(value: Int): Node = Pair(left, right.addRight(value))
+  }
+  case class ExplodeResult(node: Node, addLeft: Int, addRight: Int)
+
+  case class Number(value: Int) extends Node {
+    def explode(depth: Int): ExplodeResult = ExplodeResult(this, 0, 0)
+    def split: Node = {
+      if (value >= 10) {
+        val newLeft = value / 2
+        Pair(Number(newLeft), Number(value - newLeft))
+      }
+      else Number(value)
+    }
+    def magnitude: Int = value
+    def addLeft(value2: Int): Node = Number(value + value2)
+    def addRight(value2: Int): Node = Number(value + value2)
+  }
+
+  def problem1(input: List[String]): Int = {
+    input
+      .map(parse)
+      .reduceLeft(add)
+      .magnitude
+  }
+
+  private def add(a: Node, b: Node): Node = reduce(Pair(a, b))
 
   @tailrec
-  def magnitude(nodes: Seq[Node]): Int = {
-    if (nodes.length == 1) nodes.head.n
-    else {
-      val maxLevel = nodes.maxBy(_.level).level
-      val (start, rest :: rest2 :: rest3) = nodes.span(_.level != maxLevel)
-      magnitude((start :+ Node(3 * rest.n + 2 * rest2.n, maxLevel - 1)) ++ rest3)
-    }
+  private def reduce(node: Node): Node = {
+    val next = node.explode(4).node.split
+    if (next == node) next
+    else reduce(next)
   }
 
-  def explode(nodes: Seq[Node], i: Int): Seq[Node] = {
-    i match {
-      case 0 =>
-        val Seq(node1, node2) = nodes.tail.take(2)
-        Seq(Node(0, 3), Node(node1.n + node2.n, node2.level)) ++ nodes.drop(3)
-      case a if a < nodes.length - 2 =>
-        val Seq(node1, node2, node3, node4) = nodes.slice(i - 1, i + 3)
-        val mid = Seq(Node(node1.n + node2.n, node1.level), Node(0, 3), Node(node3.n + node4.n, node4.level))
-        nodes.take(a - 1) ++ mid ++ nodes.drop(a + 3)
-      case _ =>
-        val Seq(node1, node2) = nodes.takeRight(2)
-        nodes.take(i - 1) ++ Seq(Node(node1.n + node2.n, 3), Node(0, 3))
-    }
-  }
-
-  def split(nodes: Seq[Node], i: Int): Seq[Node] = {
-    val n = nodes(i)
-    val (node1, node2) = ((n.n.toDouble / 2).floor.toInt, (n.n.toDouble / 2).ceil.toInt)
-    nodes.take(i) ++ Seq(Node(node1, n.level + 1), Node(node2, n.level + 1)) ++ nodes.drop(i + 1)
-  }
-
-  def reduce(nodes: Seq[Node]): Seq[Node] = {
-    Iterator
-      .iterate((nodes, false)) {
-        case (nodes, _) =>
-          nodes.zipWithIndex.find(_._1.level == 4).map(_._2) match {
-            case Some(i) => (explode(nodes, i), false)
-            case None =>
-              nodes.zipWithIndex.find(_._1.n > 9).map(_._2) match {
-                case Some(i) => (split(nodes, i), false)
-                case None    => (nodes, true)
-              }
-          }
+  def parse(input: String): Node = {
+    var pos = -1
+    def parse2: Node = {
+      pos += 1
+      val first = input(pos)
+      if (first == '[') {
+        val left = parse2
+        pos += 1 // ,
+        val right = parse2
+        pos += 1 // ]
+        Pair(left, right)
       }
-      .dropWhile(!_._2)
-      .next()
-      ._1
-  }
-
-  def join(nodes1: Seq[Node], nodes2: Seq[Node]): Seq[Node] = {
-    (nodes1 ++ nodes2).map(node => Node(node.n, node.level + 1))
-  }
-
-  def binTree(s: String): Seq[Node] = {
-    @tailrec
-    def tree(acc: Seq[Node], input: Seq[Char], level: Int): Seq[Node] = {
-      input match {
-        case Nil                          => acc
-        case head +: tail if head.isDigit => tree(acc :+ Node(head.asDigit, level), tail, level)
-        case '[' +: tail                  => tree(acc, tail, level + 1)
-        case ']' +: tail                  => tree(acc, tail, level - 1)
-        case _ +: tail                    => tree(acc, tail, level)
-      }
+      else Number(first.asDigit)
     }
-    tree(Seq(), s.toSeq, -1)
+    parse2
   }
 
   def main(args: Array[String]): Unit = {
