e241b221672f2fa0eec49b66acc44659b12c5196
effati <shayan@effati.com>
2021-12-25 15:59:23 +0100

    Day 18 progress

--- src/main/scala/Day18.scala
+++ src/main/scala/Day18.scala
@@ -1,66 +1,87 @@
 import scala.annotation.tailrec
 
 object Day18 {
-  sealed trait Tree
-//  case class Node(left: Tree, right: Tree) extends Tree
-  case class Leaf(value: Int) extends Tree
-
-  val PTree = "\\[(.+)]".r
-  val PTuple = "(\\d),(\\d)".r
-  val PTuples = "(\\[.+]),(\\[.+])".r
-  val PLeft = "(\\d),(.+)".r
-  val PRight = "(.+),(\\d)".r
-  val PLeaf = "(\\d)".r
-
   case class Node(n: Int, level: Int)
 
-  def binTree2(nodes: List[Node]): Tree = {
-    def test(nodes2: List[Node], targetLevel: Int): Tree = {
-      if (targetLevel == 0) {
-        
+  def problem1(input: Seq[String]): Int = {
+    val nodes = input.map(binTree)
+    magnitude(
+      nodes.tail.foldLeft(nodes.head) { (acc, node) =>
+        println(acc)
+        reduce(join(acc, node))
       }
-    }
+    )
   }
 
-  def binTree(s: String): List[Node] = {
-    @tailrec
-    def helper(xs: List[Char], level: Int, acc: List[Node]): List[Node] = xs match {
-      case Nil => acc
-      case head :: tail if head.isDigit => helper(tail, level, acc :+ Node(head.asDigit, level))
-      case '[' :: tail => helper(tail, level + 1, acc)
-      case ']' :: tail => helper(tail, level - 1, acc)
-      case _ :: tail => helper(tail, level, acc)
-    }
-    helper(s.toList, -1, Nil)
-  }
-
-/*  def binTree(input: List[Char], stack: List[Char]): Tree = {
-    val head :: tail = input
-    if (head == '[') binTree(tail, head :: stack)
-    else if (head == ']') Node(binTree(stack, List()), binTree(tail, List()))
+  @tailrec
+  def magnitude(nodes: Seq[Node]): Int = {
+    if (nodes.length == 1) nodes.head.n
     else {
-      Node(Leaf(head.toInt), binTree(tail.tail, )
+      val maxLevel = nodes.maxBy(_.level).level
+      val (start, rest :: rest2 :: rest3) = nodes.span(_.level != maxLevel)
+      magnitude((start :+ Node(3 * rest.n + 2 * rest2.n, maxLevel - 1)) ++ rest3)
     }
-  }*/
+  }
 
- /* def binTree(input: String): Tree = {
-    input match {
-      case PTuples(left, right) => Node(binTree(left), binTree(right))
-      case PTree(tree) => binTree(tree)
-      case PTuple(left, right) => Node(binTree(left), binTree(right))
-      case PLeft(left, right) => Node(Leaf(left.toInt), binTree(right))
-      case PRight(left, right) => Node(binTree(left), Leaf(right.toInt))
-      case _ => {
-        println("wtf " + input)
-        Leaf(-1)
+  def explode(nodes: Seq[Node], i: Int): Seq[Node] = {
+    i match {
+      case 0 =>
+        val Seq(node1, node2) = nodes.tail.take(2)
+        Seq(Node(0, 3), Node(node1.n + node2.n, node2.level)) ++ nodes.drop(3)
+      case a if a < nodes.length - 2 =>
+        val Seq(node1, node2, node3, node4) = nodes.slice(i - 1, i + 3)
+        val mid = Seq(Node(node1.n + node2.n, node1.level), Node(0, 3), Node(node3.n + node4.n, node4.level))
+        nodes.take(a - 1) ++ mid ++ nodes.drop(a + 3)
+      case _ =>
+        val Seq(node1, node2) = nodes.takeRight(2)
+        nodes.take(i - 1) ++ Seq(Node(node1.n + node2.n, 3), Node(0, 3))
+    }
+  }
+
+  def split(nodes: Seq[Node], i: Int): Seq[Node] = {
+    val n = nodes(i)
+    val (node1, node2) = ((n.n.toDouble / 2).floor.toInt, (n.n.toDouble / 2).ceil.toInt)
+    nodes.take(i) ++ Seq(Node(node1, n.level + 1), Node(node2, n.level + 1)) ++ nodes.drop(i + 1)
+  }
+
+  def reduce(nodes: Seq[Node]): Seq[Node] = {
+    Iterator
+      .iterate((nodes, false)) {
+        case (nodes, _) =>
+          nodes.zipWithIndex.find(_._1.level == 4).map(_._2) match {
+            case Some(i) => (explode(nodes, i), false)
+            case None =>
+              nodes.zipWithIndex.find(_._1.n > 9).map(_._2) match {
+                case Some(i) => (split(nodes, i), false)
+                case None    => (nodes, true)
+              }
+          }
+      }
+      .dropWhile(!_._2)
+      .next()
+      ._1
+  }
+
+  def join(nodes1: Seq[Node], nodes2: Seq[Node]): Seq[Node] = {
+    (nodes1 ++ nodes2).map(node => Node(node.n, node.level + 1))
+  }
+
+  def binTree(s: String): Seq[Node] = {
+    @tailrec
+    def tree(acc: Seq[Node], input: Seq[Char], level: Int): Seq[Node] = {
+      input match {
+        case Nil                          => acc
+        case head +: tail if head.isDigit => tree(acc :+ Node(head.asDigit, level), tail, level)
+        case '[' +: tail                  => tree(acc, tail, level + 1)
+        case ']' +: tail                  => tree(acc, tail, level - 1)
+        case _ +: tail                    => tree(acc, tail, level)
       }
     }
-  }*/
+    tree(Seq(), s.toSeq, -1)
+  }
 
   def main(args: Array[String]): Unit = {
     val input = Utils.read("input18")
-    input.map(binTree).foreach(println)
-
-//    println(problem1(input))
+    println(problem1(input))
   }
 }
