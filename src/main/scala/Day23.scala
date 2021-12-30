ac9989b399726183a4844fb5eb481feade221847
effati <shayan@effati.com>
2021-12-31 00:48:18 +0100

    Day 23 progress

--- src/main/scala/Day23.scala
+++ src/main/scala/Day23.scala
@@ -1,8 +1,6 @@
 import scala.annotation.tailrec
 
 object Day23 {
-  type State = Map[Coordinate, Amphipod]
-  type Graph = Map[Coordinate, Int]
   case class Coordinate(x: Int, y: Int)
 
   case class Amphipod(energy: Int)
@@ -11,12 +9,14 @@ object Day23 {
   object C extends Amphipod(100)
   object D extends Amphipod(1000)
 
+//  case class PathData(length: Int, path: Set[Coordinate])
+
   def problem2(input: List[String]): Unit = {
-    val state: State = (for {
+    val state: Map[Coordinate, Int] = (for {
       (row, y) <- input.zipWithIndex
       (cell, x) <- row.zipWithIndex
       amphipod <- parseAmphipod(cell)
-    } yield Coordinate(x, y) -> amphipod).toMap
+    } yield Coordinate(x, y) -> amphipod.energy).toMap
 
     val hallways: Set[Coordinate] = (for {
       (row, y) <- input.zipWithIndex
@@ -37,40 +37,47 @@ object Day23 {
       .sortBy(_._1)
       .map(_._2)
       .zip(Seq(A, B, C, D))
-      .flatMap(a => a._1.map(b => (b, a._2)))
+      .flatMap(a => a._1.map(b => (b, a._2.energy)))
       .toMap
 
-    val graph = state.map { case (coordinate, amphipod) => coordinate -> amphipod.energy }
-
-    def dijsktra(graph: Graph, source: Coordinate): (Graph, Map[Coordinate, Coordinate]) = {
+    def dijsktra(start: Map[Coordinate, Int]): Map[Map[Coordinate, Int], Int] = {
       @tailrec
-      def go(active: Set[Coordinate], res: Graph, pred: Map[Coordinate, Coordinate]): (Graph, Map[Coordinate, Coordinate]) = {
-        if (active.isEmpty) (res, pred)
+      def go(stack: Set[(Map[Coordinate, Int], Int)], res: Map[Map[Coordinate, Int], Int]): Map[Map[Coordinate, Int], Int] = {
+        if (stack.isEmpty) res
         else {
-          val node = active.minBy(res)
-          val amphipod = res(node)
-          val neighbors = possibleCoordinates(hallways, state, correct, allPossible, node)
-          val alt = {
-            for {
-              n <- neighbors if cost(node, n, amphipod) < res.getOrElse(n, Int.MaxValue)
-            } yield n -> cost(node, n, amphipod)
-          }.toMap
-          val active1 = active - node ++ alt.keys
-          val preds = alt.keys.map(key => key -> node).toMap
-          go(active1, res ++ alt, pred ++ preds)
+          val currentState = stack.minBy(a => res(a._1))
+          val futureStates2 = currentState._1.map { case (c, i) =>
+            val neighbors = possibleCoordinates(hallways, currentState._1, correct, allPossible, c)
+            val replacement = currentState._1.removed(c)
+            neighbors.map(neighbor => (replacement ++ Map(neighbor -> i)) -> cost(c, neighbor, i))
+          }
+//            .filter { case (_, b) => b.nonEmpty}
+
+          val futureStates = currentState._1.flatMap { case (c, i) =>
+            val neighbors = possibleCoordinates(hallways, currentState._1, correct, allPossible, c)
+            val replacement = currentState._1.removed(c)
+            neighbors.map(neighbor => (replacement ++ Map(neighbor -> i)) -> cost(c, neighbor, i))
+          }
+
+          val alt = futureStates
+            .withFilter { case (futureState, cost) => currentState._2 + cost < res.getOrElse(futureState, Int.MaxValue) }
+            .map { case (futureState, cost) => futureState -> (currentState._2 + cost) }
+          val stack1 = stack - currentState ++ alt
+//          val preds = alt.keys.map(key => key -> node).toMap
+          go(stack1, res ++ alt)
         }
       }
-      go(Set(source), Map(source -> 1), Map.empty)
+      go(Set(start), Map(start -> 0))
     }
 
-    val dij = dijsktra(graph, Coordinate(3, 2))
+    val dij = dijsktra(state)
     println(dij)
   }
 
   def cost(current: Coordinate, next: Coordinate, energy: Int): Int =
     (Math.abs(current.x - next.x) + Math.abs(current.y - next.y)) * energy
 
-  def possibleCoordinates(hallways: Set[Coordinate], state: State, correct: State, allPossible: Set[Coordinate], start: Coordinate): Set[Coordinate] = {
+  def possibleCoordinates(hallways: Set[Coordinate], state: Map[Coordinate, Int], correct: Map[Coordinate, Int], allPossible: Set[Coordinate], start: Coordinate): Set[Coordinate] = {
     val amphipod = state(start)
     val neighbors = List((-1, 0), (1, 0), (0, -1), (0, 1))
     var path = List[Coordinate]()
